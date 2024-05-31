5918e744fc742741d3d508068ad86b4bc0229137
John Chibuzor Anih <anihcj57@gmail.com>
2024-05-31 03:31:12 +0100

    Update subheadingsData.js
    Body of the About page

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -6,16 +6,36 @@ const subheadingsData = {
                 <>
                     <p>
                        Hello, I am  CJ from the Eastern part of Nigeria in West of Africa.
-                       Coming from a business background, I am of the school of thought that "SMART INSIGHTS, MAKES SMART DECISIONS".
-                       with a strong computer science background and extensive experience in machine learning, statistical analysis, and data visualization. 
+                       My business and technical background gives me the egde, to advance into any role easily.
+                       I am of the school of thought that "SMART INSIGHTS, MAKES SMART DECISIONS".
+                       Having developed and gained industrial experiences in machine learning, statistical analysis, and data visualization. 
                        I specialize in turning data into strategic insights and process improvements. 
-                       Passionate about technology and collaboration, I drive impactful projects and enhance organizational performance with data-driven strategies.
-
-
-
+                       I am passionate about technology and collaboration, driving impactful projects and enhancing organizational performance with data-driven strategies.
                     </p>
                 </>
-            )
-        }
-    ]
-}
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "2024 Masters Degree",
+            content: (
+                <>
+                    <p>
+                        Leeds Beckett University, UK.
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2018 Bachelors Degree",
+            content: (
+                <>
+                    <p>
+                        Ebonyi State University, Nigeria.
+                    </p>
+                </>
+            ),
+        },
+    ],
+};

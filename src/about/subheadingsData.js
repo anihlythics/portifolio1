b9860dcb80ea41d9c84b9daeace2d56373c91962
John Chibuzor Anih <anihcj57@gmail.com>
2024-05-31 02:09:50 +0100

    Progress
    About 35%

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,21 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                       Hello, I am  CJ from the Eastern part of Nigeria in West of Africa.
+                       Coming from a business background, I am of the school of thought that "SMART INSIGHTS, MAKES SMART DECISIONS".
+                       with a strong computer science background and extensive experience in machine learning, statistical analysis, and data visualization. 
+                       I specialize in turning data into strategic insights and process improvements. 
+                       Passionate about technology and collaboration, I drive impactful projects and enhance organizational performance with data-driven strategies.
+
+
+
+                    </p>
+                </>
+            )
+        }
+    ]
+}
